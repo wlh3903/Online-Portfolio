14a63cc65ace40a00d39f26669f041c190e5dcae
Warner-30 <wharper17@icloud.com>
2025-12-24 23:32:24 -0500

    Started work on about me and completed writing the whole thing. Working on conatiner background and still need to make a font-change

--- online-personal-portfolio/src/Pages/AboutMe.jsx
+++ online-personal-portfolio/src/Pages/AboutMe.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navigation from "../Components/Navigation";
-import { Container } from "react-bootstrap";
-
+import { Container, Row } from "react-bootstrap";
+import './AboutMe.css'
 class AboutMe extends React.Component{
     constructor(props){
         super(props)
@@ -11,6 +11,11 @@ class AboutMe extends React.Component{
         return(
             <Container>
                 <Navigation />
+                <Container>
+                    <Row className="para" as='p'>
+                        Hi, I'm Warner Harper. I'm a second-year Software Engineering student at the Rochester Institute of Technology, enrolled in the Combined Accelerated Bachelor's/Master's Program. I expect to earn my Bachelor of Science in Software Engineering in Spring 2029 and my Master of Science in Cybersecurity in Spring 2030.
+                    </Row>
+                </Container>
             </Container>
         )
     }
