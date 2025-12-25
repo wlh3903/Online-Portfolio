0d9115c371c1bef582b86c23ae8480a528253426
Warner-30 <wharper17@icloud.com>
2025-12-25 01:30:34 -0500

    Added resume preview and button to download

--- online-personal-portfolio/src/Pages/Resume.jsx
+++ online-personal-portfolio/src/Pages/Resume.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navigation from "../Components/Navigation";
-import { Container } from "react-bootstrap";
+import { Button, Container, Row } from "react-bootstrap";
+import resumePDF from '../assets/Resume-Warner_H.pdf'
 
 class Resume extends React.Component{
     constructor(props){
@@ -11,6 +12,12 @@ class Resume extends React.Component{
         return(
             <Container>
                 <Navigation />
+                <Container>
+                    <iframe src="https://drive.google.com/file/d/1sNvtIu8usUzG3jJhbEp8_16CH0iSrSnf/preview" />
+                    <a href={resumePDF} open="online-personal-portfolio\src\assets\Resume-Warner_H.pdf" target="_blank" rel="noopener noreferrer">
+                        <Button>Download Resume</Button>
+                    </a>
+                </Container>
             </Container>
         )
     }
