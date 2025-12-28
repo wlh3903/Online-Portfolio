f952950bd049ac61776e851019d0445e9905122a
Warner-30 <wharper17@icloud.com>
2025-12-27 19:45:54 -0500

    Fixed Resume on Prod after changes

--- online-personal-portfolio/src/Pages/Resume.jsx
+++ online-personal-portfolio/src/Pages/Resume.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navigation from "../Components/Navigation";
 import { Button, Container, Ratio, Row } from "react-bootstrap";
-import resumePDF from '../assets/Resume-Warner_H.pdf'
+import resumePDF from "../../public/Resume-Warner_H.pdf"
 import "./Resume.css"
 
 class Resume extends React.Component{
@@ -16,7 +16,7 @@ class Resume extends React.Component{
                 <Ratio className="preview" aspectRatio={8.5 / 11}>
                     <iframe className="viewer" src="https://drive.google.com/file/d/1sNvtIu8usUzG3jJhbEp8_16CH0iSrSnf/preview" />
                 </Ratio>
-                <a className="button" href={resumePDF} download="online-personal-portfolio\src\assets\Resume-Warner_H.pdf" target="_blank" rel="noopener noreferrer">
+                <a className="button" href={resumePDF} download="online-personal-portfolio\public\Resume-Warner_H.pdf" target="_blank" rel="noopener noreferrer">
                     <Button variant="info">Download Resume</Button>
                 </a>
             </Container>
