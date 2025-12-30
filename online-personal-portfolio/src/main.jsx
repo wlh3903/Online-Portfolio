410ce5d42eb79c5c75eeade20783d74a296b33e9
Warner-30 <wharper17@icloud.com>
2025-12-30 13:01:38 -0500

    Seperate Resume into comp

--- online-personal-portfolio/src/main.jsx
+++ online-personal-portfolio/src/main.jsx
@@ -4,12 +4,12 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './Pages/Home'
 import About from './Pages/About'
-import Resume from './Pages/Resume'
+import ResumePage from './Pages/ResumePage'
 
 const router = createBrowserRouter([
   {path: '/', element: <Home />},
   {path: '/about', element: <About />},
-  {path: '/resume', element: <Resume />}
+  {path: '/resume', element: <ResumePage />}
 ])
 
 
